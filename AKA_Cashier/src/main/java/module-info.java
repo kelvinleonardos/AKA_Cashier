bcad8db24da505e27743916217f999198eac2174
kelvinleonardos <kelvinlsianipar@gmail.com>
2023-05-22 15:00:20 +0800

    project starter, include javafx project build with gradle

---
+++ AKA_Cashier/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.aka_cashier {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.aka_cashier to javafx.fxml;
+    exports com.example.aka_cashier;
+}
